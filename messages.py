9cd4f7f84dcde865ecc97431750928104d10f091
Zeno Romio <79644242+zenoromio@users.noreply.github.com>
2023-05-24 16:11:37 +0200

    Add files via upload

---
+++ messages.py
@@ -0,0 +1,94 @@
+# this file contains all the messages that the bot will send
+# the \ is used before special characters [-, (, ), ., =, etc.] to ignore them
+# the * is used for bold text and need the parse_mode="MarkdownV2" in the message
+
+start_message = """
+*Benvenuto nel magico mondo di Zeno❤️❤️❤️*
+Per caso lo conosci?
+
+*Perché avere una pagina Wikipedia quando puoi avere un intero bot di telegram interamente per te?*
+
+Ecco alcune funzioni che puoi usare:
+• /stats \- Un po' di statistiche varie su Zeno🔢
+
+• /hoodies \- Fatti un giro dentro l'armadio delle felpe di Zeno👕
+
+• /cameraroll \- Hai voglia di farti i cazzi miei? No problem👀
+
+• /socials \- Puoi trovare Zeno abbastanza ovunque se cerchi bene📲
+
+• /zodiac \- Che segno sono??? \(Sole, Luna e Ascendente included\)♒️
+
+• /message \- Scrivi un messaggio e firmati così so che sei passato/a⬇️⬇️⬇️"""
+
+stats_message = """
+Nome completo: *Zeno Romio*
+
+Data di nascita: *8 Febbraio 2004*
+
+Luogo di nascita: *Noventa Vicentina*
+
+Misure: *190 cm x 77 kg x 6 cm*
+
+Scuola: *Liceo Lioy \(5AT\)*
+
+Sport: *Pallacanestro*
+
+Rizz: *W*"""
+
+zodiac_message = """
+Questi sono i miei segni zodiacali 🤭🤭
+
+
+Sole☀️ in *Aquario* ♒️
+
+Luna🌕 in *Vergine* ♍️ 
+
+Ascendente💫 in *Cancro* ♋️"""
+
+social_message = """
+
+*Instagram❤️*: https://instagram\.com/zeno\.romio?igshid\=YmMyMTA2M2Y\=
+
+*Twitter💙*: https://twitter\.com/zenoromio?s\=21&t\=wDYXuQQCkAShyb9H_2teUg
+
+*Spotify💚*: https://open\.spotify\.com/user/zeno\.romio?si\=SbCDgIJdTZaMOw1tN0dkgA
+
+*Snapchat💛*: https://t\.snapchat\.com/WRstxAnA
+
+*Duolingo🇳🇱*: https://www\.duolingo\.com/profile/zeno\.romio?via\=share_profile"""
+
+captions = {
+    "1": "In memoria del filtro 3D più bello di sempre [1/32]",
+    "2": "Sono facilmente adescabile [2/32]",
+    "3": "Damn I miss football😔 [3/32]",
+    "4": "Buc-ee's TEXAS! [4/32]",
+    "5": "Se non hai mai visto \"Smetto quando voglio\", corri subito a guardarlo [5/32]",
+    "6": "Qua ero proprio america america [6/32]",
+    "7": "Sua maestà Zeno [7/32]",
+    "8": "Walter Bianco all'opera [8/32]",
+    "9": "TN ai piedi si muovono svelti [9/32]",
+    "10": "Capelli più lunghi di sempre [10/32]",
+    "11": "Sindre direttamente dalla Norvegia [11/32]",
+    "12": "Zeno x Moschino [12/32]",
+    "13": "Prom con Sid [13/32]",
+    "14": "Prom con Ace [14/32]",
+    "15": "Non nelle mie migliori condizioni [15/32]",
+    "16": "Outfit decisamente stiloso [16/32]",
+    "17": "Bollito [17/32]",
+    "18": "Il trio della Morte con Mario e Pedro [18/32]",
+    "19": "Il coach più swag della storia [19/32]",
+    "20": "Massacrato a basket [20/32]",
+    "21": "Living the AI dream [21/32]",
+    "22": "I regnanti di Orbetello [22/32]",
+    "23": "Drip da 18esimo [23/32]",
+    "24": "I modelli del Bar Alpi [24/32]",
+    "25": "Se non vai dalla trap, la trap viene da te [25/32]",
+    "26": "Questa foto mi piace da morire [26/32]",
+    "27": "Attimi appena prima della caduta di Gio [27/32]",
+    "28": "Ritorno al futuro [28/32]",
+    "29": "Gita a Napulè [29/32]",
+    "30": "Augustus Gloop [30/32]",
+    "31": "Tipica giornata di duro lavoro [31/32]",
+    "32": "Io e Olga con le moves bulgare [32/32]"
+}
